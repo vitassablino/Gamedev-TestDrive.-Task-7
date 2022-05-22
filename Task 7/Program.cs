70213340c3c614ab2b6b4276ed73c3377112c528
vitassablino <105976325+vitassablino@users.noreply.github.com>
2022-05-22 16:48:38 +0300

    Добавьте файлы проекта.

---
+++ Task 7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        uint goldAmount;
+        uint gemsAmount = 0;
+        uint gemsRate = 100;
+
+        Console.Write("Введите стартовое количество золота - ");
+        goldAmount = Convert.ToUInt32(Console.ReadLine());
+        Console.Write($"Сколько кристаллов хотите купить по курсу {gemsRate} золота за кристалл? \t");
+        gemsAmount = Convert.ToUInt32(Console.ReadLine());
+        goldAmount -= gemsRate * gemsAmount;
+        Console.WriteLine($"У вас имеется {goldAmount} золота и {gemsAmount} кристаллов");
+    }
+}
